1b0e07eee9f8623c9dbfee304a3f4b0ab564347e
Mianxiong <757134184@qq.com>
2023-01-28 16:42:43 +0800

    创建三个页面

---
+++ src/views/Labels.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Labels
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Lables'
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
