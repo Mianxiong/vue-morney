87e5f6ec7a93ada07d1fd82fbd25591702daaff8
Mianxiong <757134184@qq.com>
2023-01-31 00:00:43 +0800

    增加部分样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,7 +15,7 @@
     <div>
       <label class="notes">
         <span>备注</span>
-        <input type="text" />
+        <input type="text" placeholder="在这里输入备注"/>
       </label>
     </div>
     <div class="name">
@@ -55,4 +55,70 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+.types{
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after{
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
+}
+.notes{
+  font-size: 14px;
+  background: hsl(0, 0%, 96%);
+  display: block;
+  padding-left: 16px;
+  align-items: center;
+  .name{
+    padding-right: 16px;
+  }
+  input{
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  > .current {
+    display: flex;
+    > li{
+      background: #d9d9d9;
+      height: 24px;
+      line-height: 24px;
+      border-radius: (24px/2);
+      padding: 0 16px;
+      margin-right: 12px;
+    }
+  }
+  > .new{
+    padding-top: 16px;
+    button{
+      background: transparent;
+      border:none;
+      color:#999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+}
 </style>
