b2d67953e9695efbb32fd47325f760df664912a6
Mianxiong <757134184@qq.com>
2023-02-01 12:34:44 +0800

    添加createdAt字段和数据迁移

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,10 @@
 
 <template>
   <Layout class-prefix="layout">
-    {{record}}
-    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
-    <Types :value.sync="record.type"/>
-    <Notes @update:value="onUpdateNotes"/>
+    {{ recordList }}
+    <NumberPad :value.sync="record.amount" @submit="saveRecord" />
+    <Types :value.sync="record.type" />
+    <Notes @update:value="onUpdateNotes" />
     <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
   </Layout>
 </template>
@@ -16,38 +16,55 @@ import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component, Vue, Watch } from "vue-property-decorator";
 
-type Record = {
-  tags: string[],
-  notes: string,
-  type: string,
-  amount: number
+const version = window.localStorage.getItem("version") || "0";
+const recordList: Record[] = JSON.parse(
+  window.localStorage.getItem("recordList") || "[]"
+);
+
+if (version === "0.0.1") {
+  //数据库升级，数据迁移
+  recordList.forEach((record) => {
+    record.createdAt = new Date(0);
+  });
+  //保存数据
+  window.localStorage.setItem("recordList", JSON.stringify(recordList));
 }
+window.localStorage.setItem("version", "0.0.2");
+
+type Record = {
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number; //数据类型 object | string
+  createdAt?: Date; //类/构造函数
+};
 
 @Component({
-  components: { NumberPad, Types, Notes, Tags }
+  components: { NumberPad, Types, Notes, Tags },
 })
 export default class Money extends Vue {
   tags = ["衣", "食", "住", "行"];
-  recordList: Record[] = [];
+  recordList: Record[] = recordList;
   record: Record = {
     tags: [],
-    notes: '',
-    type: '-',
-    amount: 10
-  }
+    notes: "",
+    type: "-",
+    amount: 10,
+  };
   onUpdateTags(value: string[]) {
-    this.record.tags = value
+    this.record.tags = value;
   }
   onUpdateNotes(value: string) {
-    this.record.notes = value
+    this.record.notes = value;
   }
   saveRecord() {
-    const record2 = JSON.parse(JSON.stringify(this.record)) // 深拷贝
-    this.recordList.push(record2)
+    const record2: Record = JSON.parse(JSON.stringify(this.record)); // 深拷贝
+    record2.createdAt = new Date();
+    this.recordList.push(record2);
   }
-  @Watch('recordList')
+  @Watch("recordList")
   onRecordListChange() {
-    window.localStorage.setItem('recordlist',JSON.stringify(this.recordList))
+    window.localStorage.setItem("recordList", JSON.stringify(this.recordList));
   }
   // onUpdateType(value: string) {
   //   this.record.type = value
