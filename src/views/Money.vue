a1fd0091b8b842cccb8c1d57477d097cbc244ecd
Mianxiong <757134184@qq.com>
2023-02-02 00:24:17 +0800

    展示和创建标签

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,10 +15,12 @@ import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component, Vue, Watch } from "vue-property-decorator";
-import model from '@/model'
+import recordListModel from '@/models/recordListModel'
+import tagListModel from '@/models/tagListModel'
 
 const version = window.localStorage.getItem("version") || "0";
-const recordList = model.fetch()
+const recordList = recordListModel.fetch()
+const tagList = tagListModel.fetch()
 
 if (version === "0.0.1") {
   //数据库升级，数据迁移
@@ -42,7 +44,7 @@ window.localStorage.setItem("version", "0.0.2");
   components: { NumberPad, Types, Notes, Tags },
 })
 export default class Money extends Vue {
-  tags = ["衣", "食", "住", "行"];
+  tags = tagList;
   recordList: RecordItem[] = recordList;
   record: RecordItem = {
     tags: [],
@@ -57,13 +59,13 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    const record2: RecordItem = model.clone(this.record); // 深拷贝
+    const record2: RecordItem = recordListModel.clone(this.record); // 深拷贝
     record2.createdAt = new Date();
     this.recordList.push(record2);
   }
   @Watch("recordList")
   onRecordListChange() {
-    model.save(this.recordList)
+    recordListModel.save(this.recordList)
   }
   // onUpdateType(value: string) {
   //   this.record.type = value
